ecdbf390fba7af8f1ae24dba670a42b4beef6d23
Shadaan <Mohdshadaan.hussain@elevatek12.com>
2025-06-22 19:35:54 +0530

    Created some utils

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import mongoose from 'mongoose';
 import {DB_NAME} from "./constants.js";
 
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: "./env",
 });
-connectDB();
+connectDB()
+    .then(()=>{
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        })
+    })
+    .catch((err) => {
+        console.error("MONGO db connection failed!!! ",err);
+    });
 // import express from 'express';
 // const app = express()
 // ;(async () => {
